d059ffc581d36a6f89fb60196fe7a00556920b8b
Ling Jiang <ling.jiang@here.com>
2025-07-15 11:46:07 +0800

    add ev accessrestrictionreasons

---
+++ android-ev-accessRestrictionReasons/app/src/main/java/com/here/search/MainActivity.java
@@ -0,0 +1,177 @@
+/*
+ * Copyright (C) 2019-2025 HERE Europe B.V.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+package com.here.search;
+
+import android.content.Context;
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+
+import com.here.sdk.core.engine.AuthenticationMode;
+import com.here.sdk.core.engine.LayerConfiguration;
+import com.here.sdk.core.engine.SDKBuildInformation;
+import com.here.sdk.core.engine.SDKNativeEngine;
+import com.here.sdk.core.engine.SDKOptions;
+import com.here.sdk.core.errors.InstantiationErrorException;
+import com.here.sdk.mapview.MapError;
+import com.here.sdk.mapview.MapScene;
+import com.here.sdk.mapview.MapScheme;
+import com.here.sdk.mapview.MapView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private PermissionsRequestor permissionsRequestor;
+    private MapView mapView;
+    private SearchExample searchExample;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Usually, you need to initialize the HERE SDK only once during the lifetime of an application.
+        initializeHERESDK();
+
+        setContentView(R.layout.activity_main);
+
+        Log.d("", "HERE SDK version: " + SDKBuildInformation.sdkVersion().versionName);
+
+        // Get a MapView instance from layout.
+        mapView = findViewById(R.id.map_view);
+        mapView.onCreate(savedInstanceState);
+
+        handleAndroidPermissions();
+    }
+
+    private void initializeHERESDK() {
+        // Set your credentials for the HERE SDK.
+        String accessKeyID = "";
+        String accessKeySecret = "";
+        AuthenticationMode authenticationMode = AuthenticationMode.withKeySecret(accessKeyID, accessKeySecret);
+        SDKOptions options = new SDKOptions(authenticationMode);
+
+        options.layerConfiguration = getLayerConfigurationWithOfflineSearch();
+
+        try {
+            Context context = this;
+            SDKNativeEngine.makeSharedInstance(context, options);
+        } catch (InstantiationErrorException e) {
+            throw new RuntimeException("Initialization of HERE SDK failed: " + e.error.name());
+        }
+    }
+
+    public static LayerConfiguration getLayerConfigurationWithOfflineSearch() {
+        ArrayList<LayerConfiguration.Feature> features = new ArrayList<>();
+        features.add(LayerConfiguration.Feature.EV);
+        features.add(LayerConfiguration.Feature.OFFLINE_SEARCH);
+        LayerConfiguration layerConfiguration = new LayerConfiguration();
+        layerConfiguration.enabledFeatures = features;
+        layerConfiguration.implicitlyPrefetchedFeatures = features;
+        return layerConfiguration;
+    }
+
+    private void handleAndroidPermissions() {
+        permissionsRequestor = new PermissionsRequestor(this);
+        permissionsRequestor.request(new PermissionsRequestor.ResultListener(){
+
+            @Override
+            public void permissionsGranted() {
+                loadMapScene();
+            }
+
+            @Override
+            public void permissionsDenied() {
+                Log.e(TAG, "Permissions denied by user.");
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        permissionsRequestor.onRequestPermissionsResult(requestCode, grantResults);
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    private void loadMapScene() {
+        // Load a scene from the HERE SDK to render the map with a map scheme.
+        mapView.getMapScene().loadScene(MapScheme.NORMAL_DAY, new MapScene.LoadSceneCallback() {
+            @Override
+            public void onLoadScene(@Nullable MapError mapError) {
+                if (mapError == null) {
+                    searchExample = new SearchExample(MainActivity.this, mapView);
+                } else {
+                    Log.d(TAG, "onLoadScene failed: " + mapError);
+                }
+            }
+        });
+    }
+
+    public void searchExampleButtonClicked(View view) {
+        searchExample.onSearchButtonClicked();
+    }
+
+    public void geocodeAnAddressButtonClicked(View view) {
+        searchExample.onGeocodeButtonClicked();
+    }
+
+    @Override
+    protected void onPause() {
+        mapView.onPause();
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        mapView.onResume();
+        super.onResume();
+    }
+
+    @Override
+    protected void onDestroy() {
+        mapView.onDestroy();
+        disposeHERESDK();
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        mapView.onSaveInstanceState(outState);
+        super.onSaveInstanceState(outState);
+    }
+
+    private void disposeHERESDK() {
+        // Free HERE SDK resources before the application shuts down.
+        // Usually, this should be called only on application termination.
+        // Afterwards, the HERE SDK is no longer usable unless it is initialized again.
+        SDKNativeEngine sdkNativeEngine = SDKNativeEngine.getSharedInstance();
+        if (sdkNativeEngine != null) {
+            sdkNativeEngine.dispose();
+            // For safety reasons, we explicitly set the shared instance to null to avoid situations,
+            // where a disposed instance is accidentally reused.
+            SDKNativeEngine.setSharedInstance(null);
+        }
+    }
+}
